890d5a2175499f02dd1e21c83402c55bc59cf61b
Sigurd Ljødal <sigurd@ljodal.no>
2015-06-15 22:06:28 +0200

    Initial Commit

---
+++ SwiftTime/SwiftTime.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTime.h
+//  SwiftTime
+//
+//  Created by Sigurd Ljødal on 15.06.15.
+//  Copyright © 2015 Sigurd Ljødal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftTime.
+FOUNDATION_EXPORT double SwiftTimeVersionNumber;
+
+//! Project version string for SwiftTime.
+FOUNDATION_EXPORT const unsigned char SwiftTimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTime/PublicHeader.h>
+
+
