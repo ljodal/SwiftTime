444b79ef3992b2b7afbbeca66e6b63f02275ee1f
Sigurd Ljødal <sigurd@ljodal.no>
2015-06-20 17:55:22 +0200

    Flip isLeapYear if test

--- SwiftTime/Chronology.swift
+++ SwiftTime/Chronology.swift
@@ -39,14 +39,14 @@ class ISOChronology : Chronology {
     }
 
     func isLeapYear(year: Int64) -> Bool {
-        if year % 400 == 0 {
-            return true
-        } else if year % 100 == 0 {
+        if year % 4 != 0 {
             return false
-        } else if year % 4 == 0 {
+        } else if year % 100 != 0 {
             return true
+        } else if year % 400 != 0 {
+            return false
         } else {
-            return false
+            return true
         }
     }
 
